c152516cbdc95b4001b0248a5b5638ee25b0a82c
Nguyễn Tuấn Vinh <nguyentuanvinh1222@gmail.com>
2024-04-24 11:06:13 +0700

    A error showed up when using xTaskCreatePinnedToCore
    Memory's dumd at core 0

--- src/main.cpp
+++ src/main.cpp
@@ -1,18 +1,72 @@
-#include <Arduino.h>
+#pragma region INCLUDE SECTION
 
-// put function declarations here:
-int myFunction(int, int);
+  #include <Arduino.h>
+  #include <WiFi.h>
+  #include <WebServer.h>
+
+#pragma endregion
+
+#pragma region DEFINE SECTION
+  
+  #define SOIL_MOISTURE_PIN 25
+  #define RELAY_PIN         26
+  #define BUTTON_PIN        27
+
+#pragma endregion
+
+#pragma region DEFINE VARIABLE
+
+  volatile long pressedCount = 0;
+  volatile long rawPressedCount =0;
+  volatile long lastButtonPressMS =0;
+
+#pragma endregion
+
+#pragma region DEFINE FUNCTION
+
+  float moistureConverter(uint16_t sensorRead);
+
+  void toggleRelay(void* arg);
+
+  void ICACHE_RAM_ATTR ButtonISR();
+#pragma endregion
 
 void setup() {
   // put your setup code here, to run once:
-  int result = myFunction(2, 3);
+  Serial.begin(115200);
+  pinMode(RELAY_PIN, OUTPUT);
+  pinMode(BUTTON_PIN, INPUT_PULLDOWN);
+  attachInterrupt(BUTTON_PIN , ButtonISR, FALLING);
+  // xTaskCreatePinnedToCore(toggleRelay, "ToggleRelay", 2048, NULL , 2, NULL,0);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
+// 
+  Serial.printf("Debounce Presses: %d\n", pressedCount);
+  Serial.printf("Raw Presses: %d\n", rawPressedCount);
+  delay(5000);
 }
 
-// put function definitions here:
-int myFunction(int x, int y) {
-  return x + y;
-}
+#pragma region FUNCTION IMPLEMENTATION
+
+  float moistureConverter(uint16_t sensorRead){
+    return 100 - ((sensorRead/4095.00)*100);
+  }
+
+  void toggleRelay(void* arg){
+    
+    // if(BUTTON_PIN == HIGH){
+    //   digitalWrite(RELAY_PIN, HIGH);
+    // }
+
+  }
+
+  void ICACHE_RAM_ATTR ButtonISR(){
+    rawPressedCount ++;
+    if((millis() - lastButtonPressMS) >200) {
+      lastButtonPressMS = millis();
+      pressedCount++;
+    }
+  }
+
+#pragma endregion
